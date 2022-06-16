5d63b9983e4c4bb19e0e4c56a6fb73786ea0841a
sab59 <106371629+sab59@users.noreply.github.com>
2022-06-16 09:49:55 +0200

    ajout

--- accueil.php
+++ accueil.php
@@ -2,44 +2,21 @@
 include './include/header.php';
 ?>
 
-<section>
     <div class="title">
         <h2>Nos dernières créations :</h2>
     </div>
-    <div class="container-gallerie py-5">
-            <div class="row">
-                <div class="col-md-3 col-sm-4">
-                    <div class="card mb-3 shadow-sm">
-                        <img src="../img/img1.PNG" alt="rainbow cake" class="w-100">
-                        <div class="card-body bg-dark">
-                            <p class="card-text text-light">Rainbow cake en forme de mignon.</p>
-                        </div>
-                    </div>
+   <section class="gallerie">
+        <div class="gallerie_card">
+            <div class="card">
+                <a href="#">
+                    <div class="card_img"></div>
+                </a>
+                <div class="text">
+                    <h3>Rainbow cake en forme de mignon</h3>
                 </div>
-
-                <div class="col-md-3 col-sm-4">
-                    <div class="card mb-3 shadow-sm">
-                        <img src="../img/img2.PNG" alt="wedding cake" class="w-100">
-                        <div class="card-body bg-dark">
-                            <p class="card-text text-light">Wedding cake bleue et blanc.</p>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-md-3 col-sm-4">
-                    <div class="card mb-3 shadow-sm">
-                        <img src="../img/img3.PNG" alt="gâteau Lego" class="w-100">
-                        <div class="card-body bg-dark">
-                            <p class="card-text text-light">Gâteau Lego</p>
-                        </div>
-                    </div>
-                </div>
-
-
             </div>
-    </div>
-</section>
-
+        </div>
+   </section>
 <article class="avis">
     <div class="title">
         <h2>Les avis de nos clients :</h2>
