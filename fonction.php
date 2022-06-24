ecc30dd4da661f5a9ab88722d02175c0b727e523
Drelon Sabrina <106371629+sab59@users.noreply.github.com>
2022-06-24 11:18:45 +0200

    Delete fonction.php

--- fonction.php
+++
@@ -1,8 +0,0 @@
-<?php
-    function valid_donnees($donnees){
-        $donnees = trim($donnees);
-        $donnees = stripcslashes($donnees);
-        $donnees = htmlspecialchars($donnees);
-        return $donnees;
-    }
-?>
